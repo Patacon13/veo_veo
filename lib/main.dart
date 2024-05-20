7d16cabd3aca6d246f65ac81a4a75c99eee14e42
Valentin Fontana <valefontana15@gmail.com>
2024-05-20 10:41:24 -0300

    agregado funcionamiento de login con fireauth y ui para ingresar datos de usuario

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:veo_veo/screens/pages/login/login.dart';
 import 'package:veo_veo/screens/pages/logros/logros.dart';
-import 'package:veo_veo/screens/pages/mapa/mapa.dart';
+import 'package:veo_veo/screens/pages/scan/scan.dart';
 import 'package:veo_veo/screens/pages/perfil/perfil.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:veo_veo/screens/pages/scan/scan.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(VeoVeo());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const VeoVeo());
 }
 
 
 class VeoVeo extends StatelessWidget {
-  bool logueado = true;
+  const VeoVeo({super.key});
+
+  
+  bool sesion(){
+    return FirebaseAuth.instance.currentUser != null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,23 +29,33 @@ class VeoVeo extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-        home: logueado ? HomePage() : LoginPage(),
+        home: sesion()? HomePage() : LoginPage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  final int interfazInicial;
+
+    HomePage({Key? key, this.interfazInicial = 0}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
-  final List<Widget> _pages = [
+  late int _paginaActual;
+  final List<Widget> _paginas = [
     LogrosPage(),
     ScanPage(),
     PerfilPage(),
   ];
+
+@override
+  void initState() {
+    super.initState();
+    _paginaActual = widget.interfazInicial;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,16 +63,16 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Veo Veo'),
       ),
-      body: _pages[_currentIndex],
+      body: _paginas[_paginaActual],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
+        currentIndex: _paginaActual,
         selectedItemColor: Colors.blue,
         onTap: (int index) {
           setState(() {
-            _currentIndex = index;
+            _paginaActual = index;
           });
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.emoji_events),
             label: 'Logros',
