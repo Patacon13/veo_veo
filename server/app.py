5e2687d891416139f553b7b17f093204a52c9196
Valentin Fontana <valefontana15@gmail.com>
2024-06-10 14:27:55 -0300

    agregada ruta para recalcular vectores de imagenes

--- server/app.py
+++ server/app.py
@@ -6,7 +6,6 @@ import base64
 from google.cloud import storage
 import os
 
-
 app = Flask(__name__)
 CORS(app)
 orb = cv2.ORB_create()
@@ -18,20 +17,26 @@ bucket = 'veo-veo-9d124.appspot.com'
 directorio = 'reconocimiento/'
 diccionario = {}
 
-blobs = cliente.list_blobs(bucket, prefix=directorio)
-for blob in blobs:
-    bin_imagen = blob.download_as_string()
-    if bin_imagen: 
-        cv2_imagen = cv2.imdecode(np.frombuffer(bin_imagen, np.uint8), cv2.IMREAD_COLOR)
-        if cv2_imagen is not None: #esto es porque toma el directorio tambien, entonces hay que verificar que no sea un directorio antes de hacer la conversion
-            gray_image = cv2.cvtColor(cv2_imagen, cv2.COLOR_BGR2GRAY)
-            keypoints, descriptores = orb.detectAndCompute(gray_image, None)
-            nombre_archivo,_ = os.path.splitext(blob.name.replace(directorio, '')) 
-            diccionario[nombre_archivo] = (keypoints, descriptores)
+def cargar_imagenes_repositorio():
+    global diccionario
+    diccionario = {} 
+    blobs = cliente.list_blobs(bucket, prefix=directorio)
+    for blob in blobs:
+        bin_imagen = blob.download_as_string()
+        if bin_imagen:
+            cv2_imagen = cv2.imdecode(np.frombuffer(bin_imagen, np.uint8), cv2.IMREAD_COLOR)
+            if cv2_imagen is not None:
+                gray_image = cv2.cvtColor(cv2_imagen, cv2.COLOR_BGR2GRAY)
+                keypoints, descriptores = orb.detectAndCompute(gray_image, None)
+                nombre_archivo, _ = os.path.splitext(blob.name.replace(directorio, ''))
+                diccionario[nombre_archivo] = (keypoints, descriptores)
+            else:
+                print(f'Error decodificando: {blob.name}')
         else:
-            print(f'Error decodificando: {blob.name}')
-    else:
-        print(f'No hay data: {blob.name}')
+            print(f'No hay data: {blob.name}')
+
+# Cargar imágenes del repositorio al inicio
+cargar_imagenes_repositorio()
 
 def reconocer_imagen(image_usuario_path, diccionario_imagenes):
     nparr = np.frombuffer(base64.b64decode(image_usuario_path.split(',')[1]), np.uint8)
@@ -43,15 +48,12 @@ def reconocer_imagen(image_usuario_path, diccionario_imagenes):
     mejor_coincidencia_id = None
 
     for punto_id, (keypoint_punto, descriptor_punto) in diccionario_imagenes.items():
-        # Hacer coincidir los descriptores de características de las dos imágenes
         matches = bf.knnMatch(descriptores_usuario, descriptor_punto, k=2)
-        # Aplicar el filtro de razon de Lowe para eliminar coincidencias incorrectas
         good_matches = []
         for m, n in matches:
             if m.distance < 0.75 * n.distance:
                 good_matches.append(m)
 
-        # Calcular el puntaje de similitud basado en el número de buenas coincidencias
         score = len(good_matches) / min(len(keypoints_usuario), len(keypoint_punto))
 
         if score > max_coincidencia:
@@ -66,6 +68,11 @@ def compare_images_route():
     punto_id, score = reconocer_imagen(imagen, diccionario)
     print("coincidió con: " + str(punto_id) + " con " + str(score))
     return jsonify({'punto': punto_id, 'coincidencia': score})
+
+@app.route('/recalcular', methods=['GET']) # se podria hacer post y recibir algun token para validar 
+def recalcular_vectores():
+    cargar_imagenes_repositorio()
+    return jsonify({'status': 'Vectores recalculados con éxito'})
 
 if __name__ == '__main__':
     app.run(debug=True)
